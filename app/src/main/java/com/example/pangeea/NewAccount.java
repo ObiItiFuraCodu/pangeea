1a29f3d5f26cc853d0e33c5e4d5117dd174319e9
ObiItiFuraCodu <86513287+ObiItiFuraCodu@users.noreply.github.com>
2023-01-15 11:59:21 +0200

    Te descurci tu haibuci

--- app/src/main/java/com/example/pangeea/NewAccount.java
+++ app/src/main/java/com/example/pangeea/NewAccount.java
@@ -3,32 +3,79 @@ package com.example.pangeea;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewAccount extends AppCompatActivity {
     private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
         // Initialize Firebase Auth
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
         Button button = findViewById(R.id.button);
         EditText email = findViewById(R.id.email);
         EditText password = findViewById(R.id.password);
+        EditText name = findViewById(R.id.fullname);
+        Map<String,Object> userdat = new HashMap<>();
+        LinearLayout linear = findViewById(R.id.linearl);
+
+        db.collection("highschools")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                TextView view2 = new TextView(NewAccount.this);
+                                String s = document.getData().get("Name").toString();
+                                view2.setText(s);
+                                try{
+                                    linear.addView(view2);
+
+                                }catch(Exception e){
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,15 +85,24 @@ public class NewAccount extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if(task.isSuccessful()){
                                     FirebaseUser user = mAuth.getCurrentUser();
+                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                            .setDisplayName(name.getText().toString())
+                                            .build();
+
+                                    userdat.put("Username",name.getText().toString());
+                                    userdat.put("Hs","nimicmomentan");
+
+
 
                                 }else{
                                     Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(NewAccount.this, "Authentication failed.Try using something different",
+                                    Toast.makeText(NewAccount.this,task.getException().getMessage(),
                                             Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
             }
         });
+
     }
 }
