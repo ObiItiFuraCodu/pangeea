b1058913f00af47636e2de500b50360037c73d60
ObiItiFuraCodu <86513287+ObiItiFuraCodu@users.noreply.github.com>
2023-02-18 21:18:35 +0200

    Schimbari majore

--- app/src/main/java/com/example/pangeea/DatabaseConnector.java
+++ app/src/main/java/com/example/pangeea/DatabaseConnector.java
@@ -5,8 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,8 +30,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DatabaseConnector {
     Context context;
@@ -79,6 +85,14 @@ public class DatabaseConnector {
 
         store.collection("users").document(user.getDisplayName())
                 .set(user_data);
+        if(category.replaceAll("[^A-Za-z0-9]", "").equals("1")){
+            store.collection("highschools").document(user_highschool.replaceAll("[^A-Za-z0-9]", "")).collection("teachers").document(user.getDisplayName())
+                    .set(user_data);
+        }else{
+            store.collection("highschools").document(user_highschool.replaceAll("[^A-Za-z0-9]", "")).collection("classes").document(user_class.replaceAll("[^A-Za-z0-9]", "")).collection("pupils").document(user.getDisplayName())
+                    .set(user_data);
+
+        }
         Toast.makeText(context,"User created successfully",Toast.LENGTH_SHORT).show();
     }
     public boolean login(String email,String password){
@@ -98,7 +112,7 @@ public class DatabaseConnector {
                 });
         return log[0];
     }
-    public String[] getuserdata(){
+  /*  public String[] getuserdata(){
         String[] userdata = new String[4];
         FirebaseUser user = auth.getCurrentUser();
         userdata[0] = user.getDisplayName();
@@ -115,8 +129,10 @@ public class DatabaseConnector {
 
                     }
                 });
+
     return userdata;
-    }
+    }*/
+    //DIS SHIT DONT WORK
 
     public void import_hours(LinearLayout layout){
         FirebaseUser user = auth.getCurrentUser();
@@ -144,18 +160,18 @@ public class DatabaseConnector {
                        // Log.i("ATENTIE FRAIERE : ",user_highschool.replaceAll("[^A-Za-z0-9]", ""));
                         // Log.i("ATENTIE FRAIERE : ",user_class.replaceAll("[^A-Za-z0-9]", ""));
                         if(user_category.equals("1")){
-                            ref = dbb.getReference("hourss").child(user_highschool.replaceAll("[^A-Za-z0-9]", "")).child(user.getDisplayName());
+                            ref = dbb.getReference("hourss").child(user_highschool.replaceAll("[^A-Za-z0-9]", "")).child("teachers").child(user.getDisplayName());
 
                         }else{
-                            ref = dbb.getReference("hourss").child(user_highschool.replaceAll("[^A-Za-z0-9]", "")).child(user_class.replaceAll("[^A-Za-z0-9]", ""));
+                            ref = dbb.getReference("hourss").child(user_highschool.replaceAll("[^A-Za-z0-9]", "")).child("classes").child(user_class.replaceAll("[^A-Za-z0-9]", ""));
 
                         }
                         ref.addValueEventListener(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                 Button v = new Button(context);
-                                v.setText(snapshot.getKey());
-                                int hour_milisecs = snapshot.getValue(Integer.class);
+                                v.setText(snapshot.getValue(String.class));
+                                int hour_milisecs = 10;
                                 v.setWidth(100);
                                 v.setOnClickListener(new View.OnClickListener() {
                                     @Override
@@ -204,29 +220,86 @@ public class DatabaseConnector {
         FirebaseUser user = auth.getCurrentUser();
         FirebaseDatabase dbb = FirebaseDatabase.getInstance("https://pangeea-835fb-default-rtdb.europe-west1.firebasedatabase.app");
         DatabaseReference ref = dbb.getReference("hourss");
-        String user_data[] = getuserdata();
+        store.collection("users").document(user.getDisplayName())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
 
-        ref.child(user_data[1]).child("classes").child(class_name).child(user_data[4]).setValue(hour_ms);
-        ref.child(user_data[1]).child("teachers").child(user.getDisplayName()).child(class_name).setValue(hour_ms);
+
+                        ref.child("hourss").child((String)documentSnapshot.get("user_highschool")).child("classes").child(class_name).child((String)documentSnapshot.get("user_subject")).setValue(hour_ms);
+                        ref.child("hourss").child( (String)documentSnapshot.get("user_highschool")).child("teachers").child(user.getDisplayName()).child(class_name).setValue(hour_ms);
+                    }
+                });
+
+
 
 
     }
     public void retrieve_hour_data(String class_subject,TextView class_status){
-        String[] userdata = getuserdata();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://pangeea-835fb-default-rtdb.europe-west1.firebasedatabase.app");
-        DatabaseReference database_reference = database.getReference("hourss").child(userdata[1]).child(userdata[2]).child(class_subject);
-        database_reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                int time_in_millis = snapshot.getValue(Integer.class);
-                if(time_in_millis < System.currentTimeMillis())class_status.setText("status : active");
-            }
+        store.collection("users").document(user.getDisplayName())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
 
-            }
-        });
+
+                        DatabaseReference database_reference = database.getReference("hourss").child((String)documentSnapshot.get("user_highschool")).child((String)documentSnapshot.get("user_class")).child(class_subject);
+                        database_reference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                int time_in_millis = snapshot.getValue(Integer.class);
+                                if(time_in_millis < System.currentTimeMillis())class_status.setText("status : active");
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+
+                    }
+                });
+
+    }
+    public void retrieveclasses(Spinner spinner){
+        List<String> list = new ArrayList<String>();
+        FirebaseUser user = auth.getCurrentUser();
+        store.collection("users").document(user.getDisplayName())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                        store.collection("highsschools").document(documentSnapshot.get("user_highschool",String.class)).collection("classes")
+                                        .get()
+                                                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                                    @Override
+                                                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                                        for(int i = 0;i < queryDocumentSnapshots.size();i++){
+                                                            list.add(queryDocumentSnapshots.getDocuments().get(i).getId());
+                                                        }
+                                                    }
+                                                });
+
+                        spinner.setAdapter(new ArrayAdapter<String>(context,android.R.layout.simple_spinner_dropdown_item,list));
+                        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parent) {
+                            }
+                        });
+
+                    }
+                });
+
 
     }
 }
--- app/src/main/java/com/example/pangeea/MainActivity.java
+++ app/src/main/java/com/example/pangeea/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.pangeea;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -17,11 +31,12 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DatabaseConnector connector = new DatabaseConnector(this);
     FirebaseFirestore store = FirebaseFirestore.getInstance();
     private FirebaseAuth auth = FirebaseAuth.getInstance();
-
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
 
     @Override
@@ -31,13 +46,61 @@ public class MainActivity extends AppCompatActivity {
         TextView username = findViewById(R.id.Username);
         TextView hs = findViewById(R.id.hs);
         String highschool;
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+
+
+            return super.onOptionsItemSelected(item);
+
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+
+            case R.id.nav_logout: {
+                auth.signOut();
+                startActivity(new Intent(MainActivity.this,Login.class));
+                break;
+            }
+            case R.id.nav_add_hour: {
+
+            }
+        }
+
+        return true;
+    }
     @Override
     protected void onStart() {
-        super.onStart();
-        LinearLayout linear = findViewById(R.id.liner);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 
+        super.onStart();
+
+        LinearLayout linear = findViewById(R.id.liner);
+        connector.retrieveclasses((Spinner) navigationView.getMenu().findItem(R.id.nav_add_hour).getActionView());
         connector.import_hours(linear);
 
     }
