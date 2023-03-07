a05ad01795e53bcb9e862112625135165803620c
ObiItiFuraCodu <86513287+ObiItiFuraCodu@users.noreply.github.com>
2023-03-07 21:57:42 +0200

    Schimbari majore

--- app/src/main/java/com/example/pangeea/Task_info.java
+++ app/src/main/java/com/example/pangeea/Task_info.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Task_info extends AppCompatActivity {
-
+    DatabaseConnector connector = new DatabaseConnector(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_info);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Bundle e = getIntent().getExtras();
+        connector.retrieve_task_data_elev(e.getString("hour_ms"),findViewById(R.id.lessons_list),findViewById(R.id.submissions_list));
     }
 }
---
+++ app/src/main/java/com/example/pangeea/Task_info_proffesor.java
@@ -0,0 +1,14 @@
+package com.example.pangeea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Task_info_proffesor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_info_proffesor);
+    }
+}
