d442d156aa5b007f23617cb2f47e4f0cc45f09dd
ObiItiFuraCodu <86513287+ObiItiFuraCodu@users.noreply.github.com>
2023-03-12 22:44:33 +0200

    Schimbari majore

--- app/src/main/java/com/example/pangeea/Pupil_info.java
+++ app/src/main/java/com/example/pangeea/Pupil_info.java
@@ -5,10 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Pupil_info extends AppCompatActivity {
-
+    DatabaseConnector connector = new DatabaseConnector(this);
+    Bundle e = getIntent().getExtras();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pupil_info);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        connector.retrieve_pupil_info(e.getString("pupil_name"),e.getString("pupil_class"),findViewById(R.id.mark_list),findViewById(R.id.absence_list),findViewById(R.id.avg_mark),findViewById(R.id.absences));
+
     }
 }
