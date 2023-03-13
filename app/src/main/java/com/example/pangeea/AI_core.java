12dab9f78929b34b46f3c6fcf39ba06ece20b27b
ObiItiFuraCodu <86513287+ObiItiFuraCodu@users.noreply.github.com>
2023-03-13 19:35:28 +0200

    Schimbari majore

---
+++ app/src/main/java/com/example/pangeea/AI_core.java
@@ -0,0 +1,73 @@
+package com.example.pangeea;
+
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AI_core {
+    private String apiUrl = "https://api.openai.com/v1/completions";
+    private String accessToken = "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";
+
+    public String AI_Text(String input){
+        JSONObject requestBody = new JSONObject();
+        final String[] output = {""};
+        try {
+
+            requestBody.put("model", "text-davinci-003");
+            requestBody.put("prompt", input);
+            requestBody.put("max_tokens", 100);
+            requestBody.put("temperature", 1);
+            requestBody.put("top_p", 1);
+            requestBody.put("frequency_penalty", 0.0);
+            requestBody.put("presence_penalty", 0.0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, apiUrl, requestBody, new Response.Listener < JSONObject > () {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray choicesArray = response.getJSONArray("choices");
+                    JSONObject choiceObject = choicesArray.getJSONObject(0);
+                    output[0] = choiceObject.getString("text");
+                    Log.e("API Response", response.toString());
+                    //Toast.makeText(MainActivity.this,text,Toast.LENGTH_SHORT).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("API Error", error.toString());
+            }
+        }) {
+            @Override
+            public Map< String, String > getHeaders() throws AuthFailureError {
+                Map < String, String > headers = new HashMap< >();
+                headers.put("Authorization", "Bearer " + accessToken);
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+            @Override
+            protected Response < JSONObject > parseNetworkResponse(NetworkResponse response) {
+                return super.parseNetworkResponse(response);
+            }
+        };
+        return output[0];
+    }
+
+}
