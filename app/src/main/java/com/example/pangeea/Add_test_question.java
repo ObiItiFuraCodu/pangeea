0b92f927c2f45d10aa3bf2d1800df34af15ac288
ObiItiFuraCodu <86513287+ObiItiFuraCodu@users.noreply.github.com>
2023-04-02 20:19:32 +0200

    Schimbari majore

--- app/src/main/java/com/example/pangeea/Add_test_question.java
+++ app/src/main/java/com/example/pangeea/Add_test_question.java
@@ -16,7 +16,9 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Add_test_question extends AppCompatActivity {
-
+    Boolean a_val = false;
+    Boolean b_val = false;
+    Boolean c_val = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +27,7 @@ public class Add_test_question extends AppCompatActivity {
         types.add("File");
         types.add("Text");
         types.add("A/B/C");
-        Boolean a_val = false;
-        Boolean b_val = false;
-        Boolean c_val = false;
+
         EditText prompt = findViewById(R.id.q_prompt);
         EditText a_variant = findViewById(R.id.a_prompt);
         EditText b_variant = findViewById(R.id.b_prompt);
@@ -37,8 +37,45 @@ public class Add_test_question extends AppCompatActivity {
         Button c_valid = findViewById(R.id.c_valid);
 
         Button enter = findViewById(R.id.add_q);
+        a_valid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(a_val == false){
+                    a_val = true;
+                    a_valid.setBackgroundColor(getResources().getColor(R.color.teal_700));
 
+                }else{
+                    a_val = false;
+                    a_valid.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                }
+            }
+        });
+        b_valid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(b_val == false){
+                    b_val = true;
+                    b_valid.setBackgroundColor(getResources().getColor(R.color.teal_700));
 
+                }else{
+                    b_val = false;
+                    b_valid.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                }
+            }
+        });
+        c_valid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(c_val == false){
+                    c_val = true;
+                    c_valid.setBackgroundColor(getResources().getColor(R.color.teal_700));
+
+                }else{
+                    c_val = false;
+                    c_valid.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                }
+            }
+        });
         Spinner type = findViewById(R.id.spinner2);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(Add_test_question.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,types);
         type.setAdapter(adapter);
@@ -51,7 +88,26 @@ public class Add_test_question extends AppCompatActivity {
                         public void onClick(View v) {
                             HashMap<String,String> question = new HashMap<>();
                             question.put("prompt",prompt.getText().toString());
+                            question.put("type",types.get(position));
 
+                            if(a_val == true){
+                                question.put("valid",a_variant.getText().toString());
+                            }else{
+                                question.put("invalid",a_variant.getText().toString());
+
+                            }
+                            if(b_val == true){
+                                question.put("valid",b_variant.getText().toString());
+                            }else{
+                                question.put("invalid",b_variant.getText().toString());
+
+                            }
+                            if(c_val == true){
+                                question.put("valid",c_variant.getText().toString());
+                            }else{
+                                question.put("invalid",c_variant.getText().toString());
+
+                            }
                             Add_test test = new Add_test();
                             test.add_question(question);
 
@@ -70,6 +126,7 @@ public class Add_test_question extends AppCompatActivity {
                         public void onClick(View v) {
                             HashMap<String,String> question = new HashMap<>();
                             question.put("prompt",prompt.getText().toString());
+                            question.put("type",types.get(position));
 
                             Add_test test = new Add_test();
                             test.add_question(question);
