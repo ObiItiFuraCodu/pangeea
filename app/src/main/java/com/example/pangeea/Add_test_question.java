6f5e46966aa98ae27d305f91f83dddfc63b9eb5d
ObiItiFuraCodu <86513287+ObiItiFuraCodu@users.noreply.github.com>
2023-04-02 18:55:36 +0200

    Schimbari majore

--- app/src/main/java/com/example/pangeea/Add_test_question.java
+++ app/src/main/java/com/example/pangeea/Add_test_question.java
@@ -3,6 +3,8 @@ package com.example.pangeea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Add_test_question extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Add_test_question extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_test_question);
+        EditText prompt = findViewById(R.id.q_prompt);
+        EditText a_variant = findViewById(R.id.a_prompt);
+        EditText b_variant = findViewById(R.id.b_prompt);
+        EditText c_variant = findViewById(R.id.c_prompt);
+
+        Spinner type = findViewById(R.id.spinner2);
+
+
     }
 }
