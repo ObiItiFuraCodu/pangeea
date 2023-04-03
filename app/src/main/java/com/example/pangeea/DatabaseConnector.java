06d87d741d663454db381dcd71b6366c6b02543a
ObiItiFuraCodu <86513287+ObiItiFuraCodu@users.noreply.github.com>
2023-04-03 19:40:24 +0200

    Schimbari majore

--- app/src/main/java/com/example/pangeea/DatabaseConnector.java
+++ app/src/main/java/com/example/pangeea/DatabaseConnector.java
@@ -636,9 +636,9 @@ public class DatabaseConnector {
                                                               Log.i("SYSTEMAMSA",Integer.toString((int)System.currentTimeMillis()));
                                                               //  Log.i("HOURMILIS",Integer.toString(hour_milisecs));
                                                               if(user_category.equals("1")){
-                                                                  v.setText(value.get("class_name") + "deadline in : " +  Long.toString ((hour_milisecs - System.currentTimeMillis()) / 3600000) + " hours");
+                                                                  v.setText(value.get("class_name") + "begins in : " +  Long.toString ((hour_milisecs - System.currentTimeMillis()) / 3600000) + " hours");
                                                               }else{
-                                                                  v.setText(value.get("user_subject") + "deadline in : " +  Long.toString ((hour_milisecs - System.currentTimeMillis()) / 3600000) + " hours");
+                                                                  v.setText(value.get("user_subject") + "begins in : " +  Long.toString ((hour_milisecs - System.currentTimeMillis()) / 3600000) + " hours");
                                                               }
                                                               v.setOnClickListener(new View.OnClickListener() {
                                                                   @Override
@@ -1051,6 +1051,177 @@ public class DatabaseConnector {
                                         submissions.setAdapter(arrayAdapter);
                                     }
                                   teacher.setText(map.get("teacher").toString());
+
+                                    ai.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            context.startActivity(new Intent(context,AI_generator.class));
+                                        }
+                                    });
+
+
+
+
+
+                                }
+
+
+
+
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+
+                    }
+                });
+    }
+
+    public void retrieve_test_data_proffesor(String deadline,ListView submissions,ListView lessons_sent){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://pangeea-835fb-default-rtdb.europe-west1.firebasedatabase.app");
+        FileDownloader downloader = new FileDownloader();
+        store.collection("users").document(user.getDisplayName())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        DatabaseReference database_reference;
+
+                        database_reference = database.getReference("tasks").child((String)documentSnapshot.get("user_highschool")).child("teachers").child((String)documentSnapshot.get("Username")).child(deadline);
+
+                        database_reference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                Map<String,Object> map =  (Map<String,Object>)snapshot.getValue();
+                                if(map != null){
+                                    if((List<String>)map.get("files") != null){
+                                        List<String> lessons_list = (List<String>)map.get("files");
+                                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,lessons_list);
+                                        lessons_sent.setAdapter(adapter);
+                                    }
+                                    if(map.get("submissions") != null){
+                                        Map<String,String> submissions_map = (Map<String, String>) map.get("submissions");
+                                        List<String> presence_list = new ArrayList<>();
+                                        presence_list.addAll(submissions_map.keySet());
+                                        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(context, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,presence_list);
+                                        submissions.setAdapter(adapter2);
+                                        submissions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                try{
+                                                    downloader.saveFile(context,presence_list.get(position),"files/lessons","lesosns");
+                                                }catch(Exception e){
+                                                    Intent i = new Intent(context,FileViewer.class);
+                                                    i.putExtra("lesson_name",presence_list.get(position));
+                                                    context.startActivity(i);
+
+                                                }
+                                            }
+                                        });
+
+
+                                    }
+
+
+                                }
+
+
+
+
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+
+                    }
+                });
+
+    }
+    public void retrieve_test_data_elev(String hour_ms,ListView lessons,ListView submissions,TextView teacher,TextView ai,TextView lesson_network){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FileDownloader downloader = new FileDownloader();
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://pangeea-835fb-default-rtdb.europe-west1.firebasedatabase.app");
+        store.collection("users").document(user.getDisplayName())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        DatabaseReference database_reference;
+                        if(hour_ms == null){
+                            Log.i("YUYUYUYUYTUYUYUY","AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+                        }
+
+                        database_reference = database.getReference("tasks").child((String)documentSnapshot.get("user_highschool")).child("classes").child((String)documentSnapshot.get("user_class")).child(hour_ms);
+
+                        database_reference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                Map<String,Object> map =  (Map<String,Object>)snapshot.getValue();
+                                if(map != null){
+                                    if((List<String>)map.get("files") != null){
+                                        List<String> lessons_list = (List<String>)map.get("files");
+                                        lesson_network.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                Intent i = new Intent(context,Lesson_list.class);
+                                                i.putExtra("grade",(String)((String) documentSnapshot.get("user_class")).replaceAll("[^0-9.]", ""));
+                                                i.putExtra("main_course",map.get("title").toString());
+                                                context.startActivity(i);
+
+                                            }
+                                        });
+                                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,lessons_list);
+                                        lessons.setAdapter(adapter);
+                                        lessons.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                try{
+                                                    FirebaseStorage storage = FirebaseStorage.getInstance();
+                                                    StorageReference storageRef = storage.getReference();
+                                                    storageRef.child("lessons/" + lessons_list.get(position)).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                        @Override
+                                                        public void onSuccess(Uri uri) {
+                                                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                                                            context.startActivity(intent);
+                                                        }
+                                                    }).addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception exception) {
+                                                            // Handle any errors
+                                                        }
+                                                    });
+                                                }catch(Exception e){
+                                                    Intent i = new Intent(context,FileViewer.class);
+                                                    i.putExtra("lesson_name",lessons_list.get(position));
+                                                    context.startActivity(i);
+                                                }
+
+                                            }
+                                        });
+                                    }
+
+                                    if(map.get("submissions") != null){
+                                        Map<String,String> submissions_map = (Map<String, String>) map.get("submissions");
+                                        List<String> submission_list = new ArrayList<>();
+                                        submission_list.addAll(submissions_map.values());
+                                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,submission_list);
+                                        submissions.setAdapter(arrayAdapter);
+                                    }
+                                    teacher.setText(map.get("teacher").toString());
 
                                     ai.setOnClickListener(new View.OnClickListener() {
                                         @Override
