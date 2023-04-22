7c7464655fd286b8e3a2a337fc2ff1e8de63292f
ObiItiFuraCodu <86513287+ObiItiFuraCodu@users.noreply.github.com>
2023-04-22 10:20:30 +0200

    Schimbari majore

--- app/src/main/java/com/example/pangeea/ai/AI_core.java
+++ app/src/main/java/com/example/pangeea/ai/AI_core.java
@@ -1,11 +1,11 @@
 package com.example.pangeea.ai;
 
 import android.content.Context;
-import android.os.StrictMode;
+import android.content.Context;
 import android.util.Log;
 import android.widget.TextView;
 
-/*import com.android.volley.AuthFailureError;
+import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
@@ -14,51 +14,39 @@ import com.android.volley.Response;
 import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;*/
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.firestore.DocumentSnapshot;
-//import com.theokanning.openai.completion.CompletionChoice;
-//import com.theokanning.openai.completion.CompletionRequest;
-//import com.theokanning.openai.service.OpenAiService;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import okhttp3.Call;
-import okhttp3.FormBody;
-import okhttp3.HttpUrl;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 public class AI_core {
     private String apiUrl = "https://api.openai.com/v1/completions";
     private String accessToken = "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";
     Context context;
-    OkHttpClient client = new OkHttpClient();
+
 
     public AI_core(Context context){
         this.context = context;
     }
 
-  /*  public void AI_Text(String input, TextView responseview){
+    public void AI_Text(String input, TextView responseview){
         JSONObject requestBody = new JSONObject();
-     //   final String[] output = {""};
+        //   final String[] output = {""};
         try {
 
-            requestBody.put("model", "text-davinci-003");
+            requestBody.put("model", "text-davinci-002");
             requestBody.put("prompt", input);
-            requestBody.put("max_tokens", 100);
-            requestBody.put("temperature", 1);
-            requestBody.put("top_p", 1);
+            requestBody.put("max_tokens", 255);
+            requestBody.put("temperature", 1.0);
+            requestBody.put("top_p", 1.0);
+            requestBody.put("stop",null);
             requestBody.put("frequency_penalty", 0.0);
             requestBody.put("presence_penalty", 0.0);
         } catch (JSONException e) {
@@ -103,8 +91,8 @@ public class AI_core {
         RequestQueue queue = Volley.newRequestQueue(context);
         queue.add(request);
 
-    }*/
-    public void AI_text_3(String prompt,TextView result){
+    }
+   /* public void AI_text_3(String prompt,TextView result){
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
         StrictMode.setThreadPolicy(policy);
@@ -112,7 +100,7 @@ public class AI_core {
         RequestBody requestBody = RequestBody.create(mediaType,
                 "{\n" +
                         "  \"model\": \"text-davinci-002\",\n" +
-                        "  \"prompt\": \"kiss ma ass\",\n" +
+                        "  \"prompt\": \""+prompt+"\",\n" +
                         "  \"max_tokens\": 100,\n" +
                         "  \"temperature\": 1.0,\n" +
                         "  \"top_p\": 1.0,\n" +
@@ -132,26 +120,22 @@ public class AI_core {
         Call call = client.newCall(request);
         try {
             Response response = call.execute();
-            System.out.println(response.body().string());
+            Log.i("RESPONSE",response.body().string());
+            try {
+
+                JSONObject object = new JSONObject(String.valueOf(response.body()));
+                JSONArray array = object.getJSONArray("choices");
+                String result_text = array.getJSONObject(0).getString("text");
+                result.setText(result_text.trim());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }*/
 
-    }
-    public List<String> AI_text_2(String prompt){
-       /* OpenAiService service = new OpenAiService(accessToken);
-        CompletionRequest completionRequest = CompletionRequest.builder()
-                .prompt("Somebody once told me the world is gonna roll me")
-                .model("ada")
-                .echo(true)
-                .build();
-        List<CompletionChoice> list =  service.createCompletion(completionRequest).getChoices();
-        List<String> results = new ArrayList<>();
-        for(CompletionChoice choice : list){
-            results.add(choice.getText());
-        }*/
-        return new ArrayList<>();
-    }
     private boolean filtering_system(String course_1,String course_2){
         int nr = 0;
         if(course_1 == null || course_2 == null){
--- app/src/main/java/com/example/pangeea/test/AI_test_generator.java
+++ app/src/main/java/com/example/pangeea/test/AI_test_generator.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import com.example.pangeea.R;
 import com.example.pangeea.ai.AI_core;
+import com.example.pangeea.ai.AI_test;
 
 public class AI_test_generator extends AppCompatActivity {
 
@@ -17,11 +18,12 @@ public class AI_test_generator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ai_test_generator);
         AI_core core = new AI_core(this);
+        AI_test test = new AI_test(this);
         Bundle e = getIntent().getExtras();
         TextView result = findViewById(R.id.text_result_from);
         Button good = findViewById(R.id.good);
         Button bad = findViewById(R.id.bad);
-        core.AI_text_3("Genereaza un test bazat pe lectia" + e.getString("title") + "cu 3 intrebari fiecare avand 3 posibile raspunsuri",result);
+        core.AI_Text("Genereaza un test bazat pe lectia " + e.getString("title") + " cu 3 intrebari fiecare avand 3 posibile raspunsuri",result);
         good.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,7 +33,7 @@ public class AI_test_generator extends AppCompatActivity {
         bad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                core.AI_text_3("Genereaza un test bazat pe lectia" + e.getString("title") + "cu 3 intrebari fiecare avand 3 posibile raspunsuri",result);
+                core.AI_Text("Genereaza un test bazat pe lectia " + e.getString("title") + " cu 3 intrebari fiecare avand 3 posibile raspunsuri",result);
 
             }
         });
